8dff341def8062f990549d343d19d756564ab410
tushar kumar <official.tusharkumar@gmail.com>
2019-03-21 09:58:35 +0530

    [FIX] added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './Shared/login/login.component';
@@ -6,6 +7,7 @@ import { RecipesComponent } from './module/recipes/recipes.component';
 
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full',redirectTo: 'recipes'},
   {path: 'login', pathMatch: 'full', component: LoginComponent},
   {path: 'signup', pathMatch: 'full', component: SignupComponent},
   {path: 'recipes', pathMatch: 'full', component: RecipesComponent},
