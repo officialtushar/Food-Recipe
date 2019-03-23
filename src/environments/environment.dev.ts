51d9b937b96cf859bcaf282cbc6930badde507ec
tushar kumar <official.tusharkumar@gmail.com>
2019-03-23 18:33:57 +0530

    [FIX] updated the url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   development: true,
-  dev_url: 'http://localhost:3000/',
+  dev_url: 'http://ec2-3-88-164-91.compute-1.amazonaws.com:3000/',
   firebase_auth: {
     apiKey: "AIzaSyBR7jIHOtOXAv9WVjno4991q7zJFe2cYOQ",
     authDomain: "popularity-gainer.firebaseapp.com",
